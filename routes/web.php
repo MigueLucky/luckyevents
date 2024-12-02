6e1e75f2fcfceb41861d59d7e1a69a8c9b27791e
MigueLucky <miguejorda8@gmail.com>
2024-12-02 14:46:48 +0100

    No poder acceder sin login

--- routes/web.php
+++ routes/web.php
@@ -1,35 +1,65 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Auth;
 use App\Http\Controllers\eventoController;
 use App\Http\Controllers\foroController;
 use App\Http\Controllers\mensajeController;
 use App\Http\Controllers\UserController;
 use App\Http\Controllers\vehiculoController;
 
-Route::get('/', function () {
+
+Route::get('/login', function () {
+    if (Auth::check()) {
+        return redirect('/portada');
+    }
+
     return view('welcome');
+});
+
+Route::get('/', function () {
+    if (Auth::check()) {
+        return redirect('/portada');
+    }
+
+    return view('welcome');
+});
+
+Route::get('/portada', function () {
+    if (!Auth::check()) {
+        return redirect('/');
+    }
+
+    return view('portada');
+});
+
+Route::get('/perfil', function () {
+    if (!Auth::check()) {
+        return redirect('/');
+    }
+
+    return view('perfil');
+});
+
+Route::get('/listaForos', function () {
+    if (!Auth::check()) {
+        return redirect('/');
+    }
+
+    return view('listaForos');
+});
+
+Route::get('/listaEventos', function () {
+    if (!Auth::check()) {
+        return redirect('/');
+    }
+
+    return view('listaEventos');
 });
 
 Route::get('/terminos', function () {
     return view('terminosYPropositos');
 })->name('terminos');
-
-Route::get('/portada', function () {
-    return view('portada');
-});
-
-Route::get('/perfil', function () {
-    return view('perfil');
-});
-
-Route::get('/listaForos', function () {
-    return view('listaForos');
-});
-
-Route::get('/listaEventos', function () {
-    return view('listaEventos');
-});
 
 Route::resource('users', UserController::class);
 Route::post('/login', [UserController::class, 'login']);
